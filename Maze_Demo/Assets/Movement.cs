cb4f9837ebbce920e3e65e62aa1f766f9ae53afe
stuffjunkgames <msquared.169@gmail.com>
2017-12-09 15:28:20 -0500

    initial commit

---
+++ Maze_Demo/Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour 
+{
+
+	public float moveSpeed = 10.0f;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float forward = Input.GetAxis ("Vertical") * moveSpeed;
+		float strafe = Input.GetAxis ("Horizontal") * moveSpeed;
+		forward *= Time.deltaTime;
+		strafe *= Time.deltaTime;
+
+		transform.Translate (strafe, 0, forward);
+
+//		rb.AddForce (strafe, 0, forward);
+	}
+
+}
