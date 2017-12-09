8d92ab5e1b41df988a509bf1268fe05d8b91c6f2
stuffjunkgames <msquared.169@gmail.com>
2017-12-09 17:32:35 -0500

    added mouse look

--- Maze_Demo/Assets/Movement.cs
+++ Maze_Demo/Assets/Movement.cs
@@ -7,11 +7,15 @@ public class Movement : MonoBehaviour
 
 	public float moveSpeed = 10.0f;
 	public Rigidbody rb;
+	public Camera cam;
 
 	// Use this for initialization
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody> ();
+		cam = transform.Find ("Main Camera").GetComponent<Camera>();
+
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +27,13 @@ public class Movement : MonoBehaviour
 		strafe *= Time.deltaTime;
 
 		transform.Translate (strafe, 0, forward);
+
+		float xRotate = Input.GetAxis ("Mouse Y");
+		float yRotate = Input.GetAxis ("Mouse X");
+
+		transform.Rotate (0, yRotate, 0);
+
+		cam.transform.Rotate (-xRotate, 0, 0);
 
 //		rb.AddForce (strafe, 0, forward);
 	}
