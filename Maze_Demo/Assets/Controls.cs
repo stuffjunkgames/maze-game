494eab24ba16f327567c38ed802865a9c38a52ee
S K <seanakl5@gmail.com>
2017-12-19 01:06:53 -0600

    added missing files

---
+++ Maze_Demo/Assets/Controls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    public GameObject flashLightObject;
+    public bool flashLightOn, alreadyPressingKey;
+
+	// Use this for initialization
+	void Start () {
+        flashLightOn = true;
+        alreadyPressingKey = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F))
+        {
+            if(flashLightOn)
+            {
+                flashLightOn = false;
+            }
+            else
+            {
+                flashLightOn = true;
+            }
+            flashLightObject.SetActive(flashLightOn);
+        }
+	}
+}
